ca4ecdd2674f65a272f9fcb5cb52b98bed1ba6f7
Bhavesh Patel <b.patel@berkeley.edu>
2023-10-27 10:48:00 -0700

    fix: 🐛 docId

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'guidelines',
             position: 'left',
             label: 'Guidelines',
           },
